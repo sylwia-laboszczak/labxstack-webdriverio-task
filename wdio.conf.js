9d03f6858dc2492159599ffc466787a527ea5c0d
sylwia-laboszczak <sylwiajurczyk91@gmail.com>
2024-01-21 19:22:52 +0100

    Added chrome and firefox to config.

--- wdio.conf.js
+++ wdio.conf.js
@@ -43,15 +43,27 @@ export const config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 10,
+    maxInstances: 1,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
-    capabilities: [{
-        browserName: 'chrome'
-    }],
+    capabilities: [
+        {
+            browserName: 'chrome'
+        },
+        {
+         browserName: 'firefox'
+        },
+   
+    // {
+    //     browserName: 'firefox'
+    // }
+],
+
+
+    headless: true,
 
     //
     // ===================
